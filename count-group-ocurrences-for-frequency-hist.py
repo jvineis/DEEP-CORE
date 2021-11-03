16d8f9ee0ec77ec0e92b86466c2e5a6a18fa18a3
Joseph Henry Vineis <jvineis@gmail.com>
2021-11-03 10:20:14 -0400

    Add files via upload

---
+++ count-group-ocurrences-for-frequency-hist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+from collections import Counter
+
+outfile = open(sys.argv[2], 'w')
+depth_group = []
+for i in open(sys.argv[1], 'r'):
+    x = i.strip().split('\t')
+    depth_group.append(x[2])
+
+depth_group_count = Counter(depth_group)
+
+for key in depth_group_count.keys():
+    if depth_group_count[key] == 1:
+        next
+    else:
+        outfile.write(str(key)+'\t'+str(depth_group_count[key])+'\t'+key.split("_")[0]+'\n')
+    
